b22bee8e49be034b6e5ea068ce986d7a239b92c6
swiftv <51175303+swiftv@users.noreply.github.com>
2023-11-19 14:59:58 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+#QuizMaker
+
+#A program to make mini quizzes for onennote, or pdf printout.
+
+#input of questions and production of simple numerical question types, aimed at AQA GCSE Specification 8525
+
+#imports
+import csv
+from pypdf import PdfWriter
+
+#subroutines
+
+#import questions
+#[ "Section number", "Question", "Answer"]
+
+#output_pdf
+
+#output simple txt
+
+#user input
+#asks user for question sections to be included
+
+#main
+
